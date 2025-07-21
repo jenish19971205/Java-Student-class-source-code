9bb00fb34c6cb85908bb7884f1927da0fb52a199
jenish19971205 <jenishjeyaraj12@gmail.com>
2025-07-21 16:27:45 +0530

    Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -10,11 +10,35 @@ class CollegeStudent extends Student {
     void attendClass() {
         System.out.println("I am attending college class.");
     }
-
+//main method
     public static void main(String[] args) {
         CollegeStudent cs = new CollegeStudent();
         cs.study();
         cs.attendClass();
     }
 }
-//Multiple Inheritance
+//Multilevel Inheritance
+class Student {
+    void study() {
+        System.out.println("I am studying.");
+    }
+}
+// seperate class
+class CollegeStudent extends Student {
+    void attendClass() {
+        System.out.println("I am attending college classes.");
+    }
+}
+// seperate class
+class EngineeringStudent extends CollegeStudent {
+    void doLab() {
+        System.out.println("I am doing lab work.");
+    }
+//main method
+    public static void main(String[] args) {
+        EngineeringStudent eng = new EngineeringStudent();
+        eng.study();         // from Student
+        eng.attendClass();   // from CollegeStudent
+        eng.doLab();         // from EngineeringStudent
+    }
+}
