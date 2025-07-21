dfb35befc2cdaefe2d1bd62870470c63573e329d
jenish19971205 <jenishjeyaraj12@gmail.com>
2025-07-21 17:10:03 +0530

    Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -51,3 +51,39 @@ class EngineeringStudent extends CollegeStudent {
         eng.doLab();         // from EngineeringStudent
     }
 }
+/* Hierarchical inheritance :
+      One parent class is shared by multiple child classes.
+      One base class is shared by multiple child classes.
+      Each child gets features from the same parent, and can have its own too.
+      It’s useful when different types of things share common stuff, but do their own tasks also.
+*/
+
+class Student {
+    void study() {
+        System.out.println("I am studying.");
+    }
+}
+//seperate class
+class SchoolStudent extends Student {
+    void writeExam() {
+        System.out.println("I am writing school exams.");
+    }
+}
+//seperate class
+class CollegeStudent extends Student {
+    void attendClass() {
+        System.out.println("I am attending college classes.");
+    }
+
+ //main method   
+    public static void main(String[] args) {
+        SchoolStudent school = new SchoolStudent();
+        school.study();       // from Student
+        school.writeExam();   // from SchoolStudent
+
+        CollegeStudent college = new CollegeStudent();
+        college.study();       // from Student
+        college.attendClass(); // from CollegeStudent
+    }
+}
+
